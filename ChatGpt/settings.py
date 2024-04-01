41b2cab8bb86ab6481ddee209e2e4359abc556e9
Mohammed Ali <mohammedaalli088@gmail.com>
2024-04-01 19:36:17 +0000

    ref: settings file refactored

--- ChatGpt/settings.py
+++ ChatGpt/settings.py
@@ -9,11 +9,16 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+import datetime
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Define the temporary path
+TEMP_PATH = os.path.join(BASE_DIR, 'temp_files')
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,6 +29,7 @@ SECRET_KEY = 'django-insecure-oq$ooa)t#)ikz)m^%&mf(zu7lksebly+u6+u)(7gay6%slk%)4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +43,27 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+        #drf 3rd party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_rest_passwordreset',
+    'drf_yasg',
+    'corsheaders',
+    'dj_rest_auth',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.microsoft',
+    'allauth.socialaccount.providers.apple',
+
+    # custom apps
+    'accounts',
+    'payment',
+    'pdf',
+    'chatBox',
+    
 ]
 
 MIDDLEWARE = [
@@ -47,6 +74,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+     # Add the account middleware:
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'ChatGpt.urls'
@@ -54,7 +84,7 @@ ROOT_URLCONF = 'ChatGpt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +145,128 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:5173',
+    'https://ai-lawyer.neuracase.com',
+)
+
+CORS_ALLOWED_ORIGINS = [
+    "https://ai-lawyer.neuracase.com",
+    "https://ai-lawyer.neuracase.com",
+]
+
+CORS_ALLOW_METHODS = ['*']
+CORS_ALLOW_HEADERS = ['*']
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
+#JWT settings
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),  
+    'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=7),
+}
+
+# JWT authentication settings
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=100000),
+}
+
+AUTHENTICATION_BACKENDS = [
+    # ...
+    'allauth.account.auth_backends.AuthenticationBackend',
+    # ...
+]
+
+ACCOUNT_USERNAME_REQUIRED = False
+REST_USE_JWT = True  # or your preferred authentication method
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+
+AUTH_USER_MODEL = 'accounts.User'
+
+
+GOOGLE_REDIRECT_URL = 'https://ai-lawyer.neuracase.com'
+MICROSOFT_REDIRECT_URL = 'https://ai-lawyer.neuracase.com'
+APPlE_REDIRECT_URL = 'https://ai-lawyer.neuracase.com'
+
+# 'zquzxxqleoptctif'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'test123.moali@gmail.com'
+EMAIL_HOST_PASSWORD = 'zquzxxqleoptctif'
+
+
+# Stripe settings
+STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY', default='')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY', default='')
+STRIPE_WEBHOOK_SECRET = config('STRIPE_WEBHOOK_SECRET', default='')
+
+
+SOCIALACCOUNT_PROVIDERS = {
+    'apple': {
+        "APP": {
+            "client_id": config('SOCIAL_APPLE_CLIENT_ID'),
+            "secret": config('SOCIAL_APPLE_SECRET'),
+            "key": config('SOCIAL_APPLE_KEY'),
+            "settings": { 
+"certificate_key": """-----BEGIN PRIVATE KEY-----
+MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgCRDgZhaN/Sspvlb7
+ryE8D+YChBC2uH97BvNGOKXpHxagCgYIKoZIzj0DAQehRANCAAQdUnewuWFxDIuw
+2Mo07NB7fmGzsY+8Proz3t87y5kJuGgCb9QPTVwusFt7q9QxVHJS0uFOn6UAGKvB
+AAhUAupv
+-----END PRIVATE KEY-----"""
+
+            }
+        },
+        'SCOPE': ['profile', 'email'],
+        'AUTH_PARAMS': {'access_type': 'online'}
+    }
+}
+
+
+# Site ID
+SITE_ID = config('SITE_ID', default=1, cast=int)
+
+
+# Apple Sign In settings
+SOCIAL_AUTH_APPLE_KEY_ID = config('SOCIAL_AUTH_APPLE_KEY_ID', default='')
+SOCIAL_AUTH_APPLE_TEAM_ID = config('SOCIAL_AUTH_APPLE_TEAM_ID', default='')
+CLIENT_ID = config('CLIENT_ID', default='')
+SOCIAL_AUTH_APPLE_PRIVATE_KEY = config('SOCIAL_AUTH_APPLE_PRIVATE_KEY', default='')
+
+
+
+# PayPal settings
+PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID', default='')
+PAYPAL_CLIENT_SECRET = config('PAYPAL_CLIENT_SECRET', default='')
+PAYPAL_WEBHOOK_ID = config('PAYPAL_WEBHOOK_ID', default='')
+
